0006db8084b35da9ddb27e3e1ac6abf2688a7468
Andrew Filgueiras <andrew.lascasas@gmail.com>
2020-09-17 13:40:16 -0300

    feat: added tests for appointments and tests coverage

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,7 +1,21 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to a new create appointment', () => {});
+  it('should be able to a new create appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123');
+  });
 
   // it('should not be able to create two appointments on the same time', () => {
   //   expect(1 + 2).toBe(3);
